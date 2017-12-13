b283ef0d3d35a6a2c692f99e648a95259bd4c2bd
gundadittu <gundadittu@gmail.com>
2017-12-13 15:48:12 -0800

    dec 13

---
+++ Progress/Controllers/CoreDataHandler.swift
@@ -0,0 +1,13 @@
+//
+//  CoreDataHandler.swift
+//  Progress
+//
+//  Created by Aditya Gunda on 12/11/17.
+//  Copyright © 2017 Aditya Gunda. All rights reserved.
+//
+
+import UIKit
+
+class CoreDataHandler: NSObject {
+
+}
